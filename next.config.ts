7d59fb5069db31c1e808d08f0f547f78a37ea575
VoidNebulon <alihusnain1308@gmail.com>
2025-09-15 18:27:29 +0500

    cleaning-junk

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,3 @@
-// import type {NextConfig} from 'next';
-
-// const nextConfig: NextConfig = {
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   images: {
-//     domains: ["picsum.photos"], // whitelist external domains
-//   },
-// };
-
-// // next.config.js
-// module.exports = {
-//   async redirects() {
-//     return [
-//       {
-//         source: "/admin",
-//         destination: "/admin/index.html",
-//         permanent: false, // 👈 must be true or false
-//       },
-//     ]
-//   },
-// }
-
-
-// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
